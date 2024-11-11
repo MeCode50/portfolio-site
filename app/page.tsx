99d96851f8ecc453110aa90812918cabfd38da9e
thegrtnx <thegrtnx@duck.com>
2024-11-11 13:41:57 +0100

    ✨ imported hero file in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 //import Image from "next/image";
 
+import Hero from "@/components/Hero";
+
 export default function Home() {
 	return (
 		<main
 			className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5">
-			<div>
-				<h1>Hello Munachimso&apos;s portfolio</h1>
+			<div className="max-w-7xl w-full">
+				<Hero />
 			</div>
 		</main>
 	);
