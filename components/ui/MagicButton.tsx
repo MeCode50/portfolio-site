e3ebe9b28673e8a7052be3865e1df354fe31be9c
MeCode50 <victorycollins31@gmail.com>
2024-11-11 23:37:33 +0100

    ⚡️ styled magic buton

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-export const MagicButton = () => {
+interface MagicButtonProps {
+	title: string;
+	icon: React.ReactNode;
+	position: string;
+	handleClick: () => void;
+	otherClasses?: string;
+}
+
+export const MagicButton: React.FC<MagicButtonProps> = ({ title, icon, position, handleClick, otherClasses = "" }) => {
 	return (
 		<div>
-			<button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+			<button
+				onClick={handleClick}
+				className={`relative inline-flex h-12 w-full overflow-hidden rounded-lp p-[1px] focus:outline-none md:w-60 md:mt-10 ${otherClasses} ${position}`}>
 				<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-				<span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">Border Magic</span>
+				<span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+					{icon}
+					{title}
+				</span>
 			</button>
 		</div>
 	);
