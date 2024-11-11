d5bcd8fe68ea4ed7d8305e989c52b4da95941d10
thegrtnx <thegrtnx@duck.com>
2024-11-11 13:30:45 +0100

    ✨ styled the main tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
-import Image from "next/image";
+//import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main>
-			<h1>Welcome to Munachimso&apos;s Portfolio</h1>
-			<p>This is a portfolio that contains all my works and skills</p>
-			<Image
-				src="/images/munachimso.jpg"
-				alt="Munachimso"
-				width={500}
-				height={500}
-			/>
+		<main
+			className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
+    mx-auto sm:px-10 px-5">
+			<div>
+				<h1>Hello Munachimso&apos;s portfolio</h1>
+			</div>
 		</main>
 	);
 }
