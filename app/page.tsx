3f286f8131a1492b8d0d3645cb3f521793b47721
MeCode50 <victorycollins31@gmail.com>
2024-11-12 07:44:47 +0100

    ✨ imported floating navbar from aceternity

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 //import Image from "next/image";
 
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 export default function Home() {
 	return (
@@ -8,6 +9,7 @@ export default function Home() {
 			className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5">
 			<div className="max-w-7xl w-full">
+				<FloatingNav />
 				<Hero />
 			</div>
 		</main>
