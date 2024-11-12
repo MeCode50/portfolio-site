84443b60a2c9c5d822ac73eaffc56f75945e4e97
MeCode50 <victorycollins31@gmail.com>
2024-11-13 00:08:05 +0100

    implemented grid.tsx

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 export const Grid = () => {
-	return <div>Grid</div>;
+	return (
+		<section id="about">
+			<BentoGrid>
+				{[{ title: "title1", description: "Desc1", id: "1" }].map((item) => (
+					<BentoGridItem
+						id={item.id}
+						key={item.id}
+						title={item.title}
+						description={item.description}
+					/>
+				))}
+			</BentoGrid>
+		</section>
+	);
 };
