ffddb3a396e9d6146f8f66a63efa018fcee121d1
munachimso <victorycollins31@gmail.com>
2024-12-02 14:11:17 +0100

    🔥 styled the 6h grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -37,7 +37,7 @@ export const BentoGridItem = ({ className, title, description, img, imgClassName
 							</BackgroundGradientAnimation>
 						)}
 
-						<div></div>
+						<div className={cn(titleClassName, "group-hover/ bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10")}></div>
 					</div>
 				</div>
 			</div>
